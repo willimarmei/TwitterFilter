97661c417e57528a7363fd0c6fb1c790feb34b9f
Aaron Robeson <aaronrobeson@gmail.com>
2018-08-09 16:10:24 -0700

    Reworked data storage, should be stable now

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
 chrome.runtime.onInstalled.addListener(function()
 {
     
-    // Set Initial Conditions
-    chrome.storage.sync.set({wordList: []}, function() {
+    // // Set Initial Conditions
+    chrome.storage.sync.set({wordList: {}}, function() {
         // Complete
     });
-
     chrome.storage.sync.set({wordCount: 1}, function() {
         // Complete
     });
--- popup.js
+++ popup.js
@@ -3,21 +3,64 @@ const filterInput = document.getElementById('filterInput');
 const filterWord = document.getElementById('filterWord');
 const filterCount = document.getElementById('filterCount');
 
-var wordArray = [];
+var wordDict = {};
+
+// class Word {
+//     constructor(text, checked) {
+//         this.text = text;
+//         this.checked = checked;
+//     }
+// }
+
+var checkUncheckWord = function() {
+    // Set up
+    var wordToChange = this.id;
+    var checked = this.checked;
+    wordDict[wordToChange] = checked;
+
+    // Write to storage
+    chrome.storage.sync.set({wordList: wordDict}, function() {
+    });
+}
 
 function updateUI() {
 
     // Update the word list
     chrome.storage.sync.get('wordList', function(result) {
-
-        wordArray = result.wordList;
+        wordDict = result['wordList'];
 
         var wordsHTML = "<ul>";
-        for (var i = 0; i < wordArray.length; i++) {
-            wordsHTML = wordsHTML + ("<li>" + wordArray[i] + "</li>");
+        for (var text in wordDict) {
+            console.log(text);
+            var checked = wordDict[text];
+            if (checked == true) {
+                wordsHTML += "<input type='checkbox' class='wordCheckBox' id='" + text + "' checked> " + text + " </input></br>";
+            } else {
+                wordsHTML += "<input type='checkbox' class='wordCheckBox' id='" + text + "'>" + text +"</input></br>";
+            }
         }
-        wordsHTML = wordsHTML + ("</ul>");
+        wordsHTML += "</ul>";
         filterWord.innerHTML = wordsHTML;
+
+        // for (var i = 0; i < wordDict.length; i++) {
+        //     if (wordDict[i].checked == true) {
+        //         var start = "<input type='checkbox' class='wordCheckBox' id='" + wordDict[i].text + "' checked></input>";
+                
+        //     } else {
+        //         var start = "<input type='checkbox' class='wordCheckBox' id='" + wordDict[i].text + "'></input>";
+        //     }
+        //     wordsHTML += start + wordDict[i].text + "</br>";
+        // }
+        // wordsHTML += ("</ul>");
+        // filterWord.innerHTML = wordsHTML;
+
+        // Add listeners to the checkboxes
+        var lis = document.getElementsByClassName("wordCheckBox");
+        for (var i = 0; i < lis.length; i++) {
+            var listElement = lis[i];
+            listElement.removeEventListener("click",checkUncheckWord);
+            listElement.addEventListener("click",checkUncheckWord);
+        }
     });
 
     // Update the drop down
@@ -29,25 +72,29 @@ function updateUI() {
             filterCount.value=1;
         }
     });
+
+
 }
 
 // Initial call to update UI to setup the interface
 updateUI();
 
+// Save the current word in the input field
 function addCurrentString() {
-    wordArray.push(filterInput.value);
+
+    // const newWord = new Word(filterInput.value, true);
+    wordDict[filterInput.value] = true;
+
     filterInput.value = '';
     
-    chrome.storage.sync.set({wordList: wordArray}, function() {
-        console.log('Words: ' + wordArray);
+    chrome.storage.sync.set({wordList: wordDict}, function() {
         updateUI();
     });
 };
 
+// Save the current word limit
 function addCurrentCount() {
-
     const wordCount = filterCount.options[filterCount.selectedIndex].value;
-
     chrome.storage.sync.set({wordCount: wordCount}, function() {
         // Complete
     });
