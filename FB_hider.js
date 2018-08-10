6b496135bd04d80e9ad42e3ada303ba5a6ccf137
Aaron Robeson <aaronrobeson@gmail.com>
2018-08-10 12:08:15 -0700

    shit's broke yo

--- FB_hider.js
+++ FB_hider.js
@@ -1,46 +1,58 @@
+var words = [];
+var count = 0;
 
+function hideOrShowPosts() {
 
-// setInterval(function(){ 
-//     postsDiv = document.getElementById('recent_capsule_container');
-//     posts = postsDiv.querySelectorAll('[role="article"]');
+    // Set filter values from storage
+    
+    // console.log(words);
 
-//     for (var i = 0; i < posts.length; i++) {
-//         posts[i].style="display:none;";
-//     }
-// }, 500);
+    var posts = document.querySelectorAll('.userContentWrapper');
+
+    for (var i = 0; i < posts.length; i++) {
+        // console.log('inside a post');
+        // Analyze post
+        // console.log('\n\n POST:' + posts[i].innerHTML);
+        // break;
+        var badWords = 0;
+        var postContent = posts[i].innerHTML;
+        // console.log('test1');
+        for (var j = 0; j < words.length; j++) {
+            if (postContent.toLowerCase().match(' ' + words[j].toLowerCase() + ' ')) {
+                // console.log('found a word');
+                badWords++;
+            }
+        }
+        // console.log('test2');
+        console.log('Bad words: ' + badWords + ' Min count: ' + count);
+        if (badWords >= count) {
+            console.log('Hiding: ' + document.querySelectorAll('.userContentWrapper')[i].style);
+            // posts[i].style.display="none";
+            document.querySelectorAll('.userContentWrapper')[i].setAttribute("style", "background-color:pink");
+            // posts[i].style.display = 'none';
+            // document.querySelectorAll('.userContentWrapper')[i].style.background-color = 'pink';
+            console.log('Hiding: ' + document.querySelectorAll('.userContentWrapper')[i].style);
+            // posts[i].style='display: none;';
+            // posts[i].setAttribute("background-color", "pink");
+            // posts[i].setAttribute("display", "none");
+            // document.getElementsByClassName('userContentWrapper')[0].setAttribute("display", "none");
+            // document.getElementsByClassName('userContentWrapper')[0].style.display="none";
+        } else {
+            console.log('Showing');
+            // posts[i].style.display = 'block';
+            // posts[i].style='display: block;';
+            // posts[i].setAttribute("display", "block");
+        }
+    }
+}
+
 
 function updateContent() {
     chrome.storage.sync.get('wordList', function(result) {
         chrome.storage.sync.get('wordCount', function(result2) {
-
-            // Set filter values from storage
-            var words = Object.keys(result['wordList']);
-            var count = result2.wordCount;
-
-
-            posts = document.getElementsByClassName('userContentWrapper');
-
-            for (var i = 0; i < posts.length; i++) {
-                // Analyze post
-                // console.log('\n\n POST:' + posts[i].innerHTML);
-                // break;
-                var badWords = 0;
-                var postContent = posts[i].innerHTML;
-                for (var j = 0; j < words.length; j++) {
-                    if (postContent.toLowerCase().match(' ' + words[j].toLowerCase() + ' ')) {
-                        badWords++;
-                    }
-                }
-                if (badWords >= count) {
-                    // posts[i].style='background-color: pink;';
-                    posts[i].style='display: none;';
-                    // posts[i].setAttribute("background-color", "pink");
-                    // posts[i].setAttribute("display", "none");
-                } else {
-                    posts[i].style='display: block;';
-                    // posts[i].setAttribute("display", "block");
-                }
-            }
+            words = Object.keys(result['wordList']);
+            count = result2.wordCount;
+            hideOrShowPosts();
         });
     });
 }
@@ -49,15 +61,4 @@ updateContent();
 
 setInterval(function(){ 
     updateContent();
-}, 1000);
-
-// // Load word list
-// chrome.storage.sync.get('wordList', function(result1) {
-//     // Load word count limit
-//     chrome.storage.sync.get('wordCount', function(result2) {
-//         wordDict = Object.keys(result1['wordList']);
-//         wordCount = result2.wordCount;
-//         alert('Filter words:' + wordDict + ' count:' + wordCount);
-
-//     });
-// });
+}, 2000);
